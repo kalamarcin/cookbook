a36698cf648050acb6f4853e37c413ab1e98f413
Marcin Kala <kalamarcin1@gmail.com>
2022-11-03 16:37:02 +0100

    useFetch

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,20 @@
+import { useFetch } from '../../hook/useFetch'
+
 // styles
 import './Home.css'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+	const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+
+	return (
+		<div className="home">
+			{error && <p className="error">{error}</p>}
+			{isPending && <p className="loading">Loading...</p>}
+			{data && data.map(recipe => (
+      <h2 key={recipe.id}>{recipe.title}</h2>)
+      )}
+		</div>
+	)
 }
 
 export default Home
